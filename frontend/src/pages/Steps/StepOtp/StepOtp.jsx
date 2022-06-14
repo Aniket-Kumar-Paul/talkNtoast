60ebb1e48e7fdd3e83d0b11216ae0486c994e8c7
Aniket <aniketkumarpaul1@gmail.com>
2022-06-14 13:44:02 +0530

    added authentication, phone/email toggle & otp

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../../components/shared/Button/Button';
+import Card from '../../../components/shared/Card/Card';
+import TextInput from '../../../components/shared/TextInput/TextInput';
+import styles from './StepOtp.module.css'
 
-const StepOtp = ({onNext}) => {
+const StepOtp = ({ onNext }) => {
+  const [otp, setOtp] = useState('');
+  function next() {
+    
+  }
   return (
-    <div>
-      <div>StepOtp</div>
-      <button onClick={onNext}>Next</button>
+    <div className={styles.cardWrapper}>
+
+      <Card title="Enter the code we just texted you" icon="lock.png">
+        <TextInput value={otp} onChange={(e) => setOtp(e.target.value)} /> {/*e means event*/}
+        <div>
+          <div className={styles.actionButtonWrap}>
+            <Button onClick={next} text="Next" />
+          </div>
+
+          <p className={styles.bottomParagraph}>
+            By entering your number, you're agreeing to our Terms of Service and Privacy Policy. Thanks!
+          </p>
+        </div>
+      </Card>
     </div>
   )
 }
--- frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Email from './Email/Email';
+import Phone from './Phone/Phone';
+import styles from "./StepPhoneEmail.module.css"
+
+const phoneEmailMap = {
+  phone: Phone,
+  email: Email
+}
 
 const StepPhoneEmail = ({ onNext }) => {
+  const [type, setType] = useState('phone');
+  const Component = phoneEmailMap[type];
+
   return (
-    <div>
-      <div>StepPhoneEmail</div> 
-      <button onClick={onNext}>Next</button>
-    </div>
+    <>
+      <div className={styles.cardWrapper}>
+        <div>
+          <div className={styles.buttonWrap}>
+            <button
+              className={`${styles.tabButton} ${type === 'phone' ? styles.active : ''}`}
+              onClick={() => setType('phone')}>
+              <img src="/images/iphone.png" alt="phone" />
+            </button>
+            <button
+              className={`${styles.tabButton} ${type === 'email' ? styles.active : ''}`}
+              onClick={() => setType('email')}>
+              <img src="/images/mail.png" alt="mail" />
+            </button>
+          </div>
+          <Component onNext={onNext} />
+        </div>
+
+      </div>
+    </>
   )
 }
 
